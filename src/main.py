dfd807212ca126b01eda3985d8aebfc8bb73d70f
chinmaySanklipur <chinmaysanklipur@gmail.com>
2024-11-06 20:23:16 -0600

    first commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Chinm                                                        #
+# 	Created:      11/6/2024, 8:19:56 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
